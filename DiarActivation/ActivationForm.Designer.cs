2ce3f65d6a46006649d49ca2ae6bef666bd292ad
Sergey <lukshin.sa@yandex.ru>
2017-10-17 15:04:04 +0300

    1

---
+++ DiarActivation/ActivationForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace DiarActivation
+{
+    partial class ActivationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ActivationForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.teSerialNumber = new System.Windows.Forms.TextBox();
+            this.teActivationCode = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Серийный номер:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ключ активации:";
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(489, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Закрыть";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // teSerialNumber
+            // 
+            this.teSerialNumber.Location = new System.Drawing.Point(178, 16);
+            this.teSerialNumber.Name = "teSerialNumber";
+            this.teSerialNumber.Size = new System.Drawing.Size(448, 27);
+            this.teSerialNumber.TabIndex = 4;
+            this.teSerialNumber.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // teActivationCode
+            // 
+            this.teActivationCode.Location = new System.Drawing.Point(178, 59);
+            this.teActivationCode.Multiline = true;
+            this.teActivationCode.Name = "teActivationCode";
+            this.teActivationCode.ReadOnly = true;
+            this.teActivationCode.Size = new System.Drawing.Size(448, 82);
+            this.teActivationCode.TabIndex = 5;
+            // 
+            // ActivationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button2;
+            this.ClientSize = new System.Drawing.Size(639, 193);
+            this.Controls.Add(this.teActivationCode);
+            this.Controls.Add(this.teSerialNumber);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ActivationForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Получить ключ активации";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox teSerialNumber;
+        private System.Windows.Forms.TextBox teActivationCode;
+    }
+}
+
---
+++ DiarActivation/ActivationForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DiarActivation
+{
+    public partial class ActivationForm : Form
+    {
+        public ActivationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            byte[] key = ASCIIEncoding.ASCII.GetBytes("DIAR");
+            RC4 encoder = new RC4(key);
+            byte[] testBytes = ASCIIEncoding.ASCII.GetBytes(teSerialNumber.Text);
+            byte[] result2 = encoder.Encode(testBytes, testBytes.Length);
+            string encryptedString = encoder.GetByteString(result2);
+
+            teActivationCode.Text = encryptedString;
+        }
+    }
+}
